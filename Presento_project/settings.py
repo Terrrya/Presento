7d61073bd1457cf237e9416da5093b7e85ddc634
Denys Tereshchuk <terrya@ukr.net>
2023-03-25 17:02:22 +0400

    Added user model

--- Presento_project/settings.py
+++ Presento_project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qqidy6#5wt_0ya*lhit=vd1n=jhs_p2(pq*s%wu)2n-1er)u@c"
+SECRET_KEY = (
+    "django-insecure-qqidy6#5wt_0ya*lhit=vd1n=jhs_p2(pq*s%wu)2n-1er)u@c"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,8 +58,7 @@ ROOT_URLCONF = "Presento_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -102,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
