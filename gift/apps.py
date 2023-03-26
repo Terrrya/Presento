f00f233eec1c94c512fd52e346150abcb5509741
Denys Tereshchuk <terrya@ukr.net>
2023-03-27 01:38:57 +0400

    Renamed app presento in gift

--- gift/apps.py
+++ gift/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PresentoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "presento"
+    name = "gift"
--- presento_service/settings.py
+++ presento_service/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    "presento",
+    "gift",
     "user",
 ]
 
