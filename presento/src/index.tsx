a6b73740275635249a708980f27e32a79e888fff
OksanaBaloh <obaloh1108@gmail.com>
2023-03-26 18:35:01 +0300

    Initial commit

---
+++ presento/src/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+import './styles/index.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { App } from './App';
+
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(<App />);
