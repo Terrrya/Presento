aeff84c5637d8f1185fd215a2d76dc0cda37511b
Denys Tereshchuk <terrya@ukr.net>
2023-03-28 15:06:39 +0400

    Updated settings.py

--- presento_service/settings.py
+++ presento_service/settings.py
@@ -95,8 +95,7 @@ DATABASES = {
 
 # dj-database-url
 db_from_env = dj_database_url.config(conn_max_age=500)
-if not DEBUG:
-    DATABASES["default"].update(db_from_env)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
