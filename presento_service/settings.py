753fef3ce0c260aa7f8a01bead47964a40a27677
Denys Tereshchuk <terrya@ukr.net>
2023-03-29 15:18:59 +0400

    Added django cors headers

--- presento_service/settings.py
+++ presento_service/settings.py
@@ -49,12 +49,14 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "gift",
     "user",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -149,3 +151,5 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ]
 }
+
+CORS_ORIGIN_WHITELIST = "test-presento.onrender.com"
