d474aaf5ae260deae2c48bac65873c238231dd4e
Denys Tereshchuk <terrya@ukr.net>
2023-04-03 15:06:25 +0400

    Removed CreateTokenView & authentication_classes, fixed imports

--- user/views.py
+++ user/views.py
@@ -1,10 +1,7 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,17 +14,7 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     """Update user witch already login"""
 
     serializer_class = UserSerializer
-    # authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
-
-
-class CreateTokenView(ObtainAuthToken):
-    """
-    Create token for user when login
-    """
-
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
