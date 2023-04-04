fe91606e03b938877446ab31b62f68bc0905972f
Denys Tereshchuk <terrya@ukr.net>
2023-04-04 13:37:26 +0400

    Added GiftViewSet

--- gift/views.py
+++ gift/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from gift.models import Gift
+from gift.serializers import GiftSerializer
+
+
+class GiftViewSet(viewsets.ModelViewSet):
+    serializer_class = GiftSerializer
+    queryset = Gift.objects.all()
