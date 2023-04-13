fa13ccfe2700b1d97f0fc3ab3bf357474d9d4a4c
Denys Tereshchuk <terrya@ukr.net>
2023-04-13 17:22:45 +0400

    Updated settings for AWS bucket S3

--- presento_service/settings.py
+++ presento_service/settings.py
@@ -156,11 +156,11 @@ if os.getenv("USE_S3"):
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    # AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     # AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-    AWS_LOCATION = "media"
+    # AWS_LOCATION = "media"
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     MEDIA_URL = "/media/"
