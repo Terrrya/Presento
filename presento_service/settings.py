81cf372e7bd24384f06e13143f19adf615f40cbc
Denys Tereshchuk <terrya@ukr.net>
2023-04-13 17:03:14 +0400

    Added settings for AWS bucket S3

--- presento_service/settings.py
+++ presento_service/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "gift",
     "user",
     "corsheaders",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -150,8 +151,20 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "/vol/web/media"
+if os.getenv("USE_S3"):
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    # AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    # AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    AWS_LOCATION = "media"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+else:
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = "/vol/web/media"
 # MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
