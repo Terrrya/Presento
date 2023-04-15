f7d901c3c7a2db1cdda766102842b3fd4f126526
Denys Tereshchuk <terrya@ukr.net>
2023-04-15 22:26:43 +0400

    Added nginx server & settings for static files

--- presento_service/settings.py
+++ presento_service/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if os.getenv("USE_S3") == "True":
     # aws settings
